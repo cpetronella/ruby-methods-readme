f8dc63ec8b4229d26dc92eae12c7447fb93fe93e
Christopher <christopher.petronella@gmail.com>
2020-11-11 11:53:43 -0500

    commit tree 53d81d041c884a9ec2b319628f664955544c4d0c

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,19 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+phrase = "Hello World!"
+def say_hello_world_ten_times
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+end
+say_hello_world_ten_times
+
